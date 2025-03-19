ceb10c90f9e06688d98b2343aa1166ce48dd4366
Gertinjo <gerticalaj2013@gmail.com>
2025-03-19 19:06:53 +0100

    2last part

--- main.js
+++ main.js
@@ -18,3 +18,31 @@ $('#btn').click(function(){
 $('#btn2').on('click', function(){
     console.log("Second Button ");
 })
+$('#Mydiv').click(function(){
+
+    $(this).addClass('clicked')
+})
+.find('span')
+.attr('tittle', 'Hover Over Me')
+
+$('#btn3').click(function(){
+
+    $('.hidden').show('slow')
+});
+$('#btn4').click(function(){
+
+    $('.hidden').hide('slow')
+});
+$('#square').click(function(){
+    $('#square').animate({
+        'width': '200px',
+        'height': '200px'
+    })
+})
+$('.testing').click(function(){
+    $('.testing').animate({
+        'left':  '200px',
+        'opacity':  '0.25',
+        'font-size': '30px'
+    })
+})
